ec3f10a1614fa48a7a3358e5bc07180abbf84f97
Artem-WebDeveloper <ave_art@icloud.com>
2025-08-28 17:17:40 +0300

    task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  if (!matrix || matrix.length === 0) return [];
+  return matrix
+    .map((array, i) =>
+      i % 2 === 0 ? array.sort((a, b) => a - b) : array.sort((a, b) => b - a)
+    )
+    .flat();
 };
